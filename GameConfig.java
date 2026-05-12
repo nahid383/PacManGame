5cc59caa4646c28ac21124f0fe56787f669a30a2
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-12 15:59:24 +0600

    Created game configuration java file

---
+++ GameConfig.java
@@ -0,0 +1,9 @@
+public class GameConfig{
+    public static final int ROW_COUNT = 21;
+    public static final int COLUMN_COUNT = 19;
+    public static final int TILE_SIZE = 32;
+
+    public static final int BOARD_WIDTH = COLUMN_COUNT*TILE_SIZE;
+    public static final int BOARD_HEIGHT = ROW_COUNT*TILE_SIZE;
+    
+}
