0f2facd0b14d89f26058e05a5d930baf6cfb6e4f
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-12 15:05:30 +0600

    Added Game panel in the main java file

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Pac Man");
-        
+        GamePanel gamePanel = new GamePanel();
     }
 }
