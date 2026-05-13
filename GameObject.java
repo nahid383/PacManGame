4fa51da10395cacfa4b155066836714999dd3dcb
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-13 09:41:51 +0600

    2nd modification in gameobject.java file

--- GameObject.java
+++ GameObject.java
@@ -12,4 +12,14 @@ public class GameObject{
         this.height = height;
     }
 
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public Image getImage() { return image; }
+
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setImage(Image image) { this.image = image; }
+
 }
