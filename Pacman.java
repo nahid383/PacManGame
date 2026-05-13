28019f86cac80349b7a7522f32912f6286b64471
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-14 01:13:59 +0600

    Modified again in pacman.java file

--- Pacman.java
+++ Pacman.java
@@ -4,4 +4,16 @@ public class Pacman extends Character{
     public Pacman(Image image, int x, int y, int w, int h){
         super(image, x, y, w, h);
     }
+
+    @Override 
+    public void move() {
+        setX(getX() + velocityX);
+        setY(getY() + velocityY);
+    }
+
+    @Override
+    public void updateDirection(char d){
+        direction = d;
+        
+    }
 }
