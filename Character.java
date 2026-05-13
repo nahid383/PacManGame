af5bd840b3cbcd4314b311319a6516f6db9467aa
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-14 00:56:40 +0600

    modified or created code in character.java file

--- Character.java
+++ Character.java
@@ -1,3 +1,21 @@
-public abstract class Character {
-    
+import java.awt.Image;
+
+public abstract class Character extends GameObject implements Movable{
+    protected int velocityX, velocityY;
+    protected int startX, startY;
+    protected char direction = 'R';
+
+    public Character(Image image, int x, int y, int w, int h){
+        super(image, x, y, w, h);
+        startX = x;
+        startY = y;
+
+    }
+
+    public void reset() {
+        setX(startX);
+        setY(startY);
+        velocityX = 0;
+        velocityY = 0;
+    }
 }
