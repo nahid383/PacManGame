8adabc844522213ef7ce45248891c9af26a2cf37
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-16 09:34:53 +0600

    Created the gamepanel method

--- GamePanel.java
+++ GamePanel.java
@@ -2,10 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import  javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
     private Image wallImage;
-    private Image blueGhost, orangeGhost, pinkGhost, redghost;
+    private Image blueGhost, orangeGhost, pinkGhost, redGhost;
     private Image pacUp, pacDown, pacLeft, pacRight;
     private Image powerFoodImage;
     private Image cherryImage;
@@ -21,5 +22,17 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 
     private char[] directions = {'U', 'D', 'L', 'R'};
     
+    public GamePanel(){
+        setPreferredSize(new Dimension(GameConfig.BOARD_WIDTH, GameConfig.BOARD_HEIGHT));
 
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
+
+        loadImages();
+        loadMap();
+
+        timer = new Timer(50, this);
+        timer.start();
+    }
 }
