a4e2532c4796169cb72704897e88571986861481
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-05-17 20:16:57 +0600

    initial complete of the project

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Pac Man");
         GamePanel gamePanel = new GamePanel();
+
+        frame.add(gamePanel);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+        gamePanel.requestFocus();
     }
 }
